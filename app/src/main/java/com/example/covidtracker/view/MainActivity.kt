cf4e5e58a0528acf920613fb43f6b01c71a1b292
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-12-15 23:38:41 +0700

    chore: refactor folder

--- app/src/main/java/com/example/covidtracker/view/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/view/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.example.covidtracker
+package com.example.covidtracker.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.covidtracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-        
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
