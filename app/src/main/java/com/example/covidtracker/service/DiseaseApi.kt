f70f44c3e6bad148d665a1704112ced2a73f94da
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-12-16 17:44:52 +0700

    feat: add data fetcher

--- app/src/main/java/com/example/covidtracker/service/DiseaseApi.kt
+++ app/src/main/java/com/example/covidtracker/service/DiseaseApi.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -24,11 +25,11 @@ class DiseaseApi {
             .build()
     }
 
-    fun getService(): Response {
+    private fun getService(): Response {
         return getRetrofit().create(Response::class.java)
     }
 
-    interface Response {
+    private interface Response {
         @GET("continents")
         fun getContinents(): Call<List<Continent>>
 
@@ -40,5 +41,69 @@ class DiseaseApi {
 
         @GET("countries/{country}")
         fun getCountry(@Path("country") country: String): Call<Country>
+    }
+
+    fun getCountries(callback: (List<Country>?) -> Unit) {
+        getService().getCountries().enqueue(object : Callback<List<Country>> {
+            override fun onResponse(
+                call: Call<List<Country>>,
+                response: retrofit2.Response<List<Country>>
+            ) {
+                val data = response.body()!!
+                callback(data)
+            }
+
+            override fun onFailure(call: Call<List<Country>>, t: Throwable) {
+                callback(null)
+            }
+        })
+    }
+
+    fun getContinents(callback: (List<Continent>?) -> Unit) {
+        getService().getContinents().enqueue(object : Callback<List<Continent>> {
+            override fun onResponse(
+                call: Call<List<Continent>>,
+                response: retrofit2.Response<List<Continent>>
+            ) {
+                val data = response.body()!!
+                callback(data)
+            }
+
+            override fun onFailure(call: Call<List<Continent>>, t: Throwable) {
+                callback(null)
+            }
+        })
+    }
+
+    fun getCountry(country: String, callback: (Country?) -> Unit) {
+        getService().getCountry(country).enqueue(object : Callback<Country> {
+            override fun onResponse(
+                call: Call<Country>,
+                response: retrofit2.Response<Country>
+            ) {
+                val data = response.body()!!
+                callback(data)
+            }
+
+            override fun onFailure(call: Call<Country>, t: Throwable) {
+                callback(null)
+            }
+        })
+    }
+
+    fun getContinent(continent: String, callback: (Continent?) -> Unit) {
+        getService().getContinent(continent).enqueue(object : Callback<Continent> {
+            override fun onResponse(
+                call: Call<Continent>,
+                response: retrofit2.Response<Continent>
+            ) {
+                val data = response.body()!!
+                callback(data)
+            }
+
+            override fun onFailure(call: Call<Continent>, t: Throwable) {
+                callback(null)
+            }
+        })
     }
 }
