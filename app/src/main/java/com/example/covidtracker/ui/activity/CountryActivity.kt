aaaaca85b0c0e134fc39ca8cc394e4c625c830a0
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-12-17 17:27:08 +0700

    feat: create new activity

---
+++ app/src/main/java/com/example/covidtracker/ui/activity/CountryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.covidtracker.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covidtracker.R
+
+class CountryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_country)
+    }
+}
