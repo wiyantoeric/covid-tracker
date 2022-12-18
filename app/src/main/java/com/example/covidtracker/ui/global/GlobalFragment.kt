80541ae3acfa5edf7e4442c4736bebcc38f23164
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2022-12-18 19:44:29 +0700

    feat: update global activity

--- app/src/main/java/com/example/covidtracker/ui/global/GlobalFragment.kt
+++ app/src/main/java/com/example/covidtracker/ui/global/GlobalFragment.kt
@@ -1,14 +1,20 @@
 package com.example.covidtracker.ui.global
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.covidtracker.databinding.FragmentHomeBinding
+import com.example.covidtracker.data.global
 import com.example.covidtracker.databinding.FragmentGlobalBinding
+import numberFormat
+import org.eazegraph.lib.models.BarModel
+import org.eazegraph.lib.models.PieModel
+import org.eazegraph.lib.models.StackedBarModel
+
 
 class GlobalFragment : Fragment() {
 
@@ -29,11 +35,56 @@ class GlobalFragment : Fragment() {
         _binding = FragmentGlobalBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        globalViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        globalViewModel.fetchData {
+            updateUi(_binding)
         }
+
         return root
+    }
+
+    private fun updateUi(binding: FragmentGlobalBinding?) {
+        binding!!.progressCircular.visibility = View.GONE
+        binding.backdrop.visibility = View.GONE
+
+        val pieChart = binding.pieChart
+
+        val case = global!!.cases
+        val recovered = global!!.recovered
+        val death = global!!.deaths
+
+        val caseModel = PieModel("Total Case", case!!.toFloat(), Color.parseColor("#6D9886"))
+        val recoveredModel = PieModel("Recovered", recovered!!.toFloat(), Color.parseColor("#F2E7D5"))
+        val deathModel = PieModel("Deaths", death!!.toFloat(), Color.parseColor("#393E46"))
+
+        binding.cases.text = numberFormat(case)
+        binding.recovered.text = numberFormat(recovered)
+        binding.death.text = numberFormat(death)
+
+        pieChart.addPieSlice(caseModel)
+        pieChart.addPieSlice(recoveredModel)
+        pieChart.addPieSlice(deathModel)
+
+        pieChart.showContextMenu()
+        pieChart.startAnimation()
+
+        val stackedChart = binding.stackedChart
+        val stackedBarModel = StackedBarModel("Today Cases")
+
+        val todayCase = global!!.todayCases
+        val todayRecovered = global!!.todayRecovered
+        val todayDeath = global!!.todayDeaths
+
+        binding.todayCases.text = numberFormat(todayCase ?: 0)
+        binding.todayRecovered.text = numberFormat(todayRecovered ?: 0)
+        binding.todayDeath.text = numberFormat(todayDeath ?: 0)
+
+        stackedBarModel.addBar(BarModel("New Case", todayCase!!.toFloat(), Color.parseColor("#6D9886")))
+        stackedBarModel.addBar(BarModel("Today Recovered", todayRecovered!!.toFloat(), Color.parseColor("#F2E7D5")))
+        stackedBarModel.addBar(BarModel("Today Deaths", todayDeath!!.toFloat(), Color.parseColor("#393E46")))
+
+        stackedChart.addBar(stackedBarModel)
+
+        stackedChart.startAnimation()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/covidtracker/ui/global/GlobalViewModel.kt
+++ app/src/main/java/com/example/covidtracker/ui/global/GlobalViewModel.kt
@@ -3,11 +3,24 @@ package com.example.covidtracker.ui.global
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.covidtracker.data.continents
+import com.example.covidtracker.data.global
+import com.example.myapplication.DiseaseApi
 
 class GlobalViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
     }
+
+    fun fetchData(callback: () -> Unit) {
+        if (global == null) DiseaseApi().getGlobal { data ->
+            global = data!!
+            callback()
+        } else {
+            callback()
+        }
+    }
+
     val text: LiveData<String> = _text
 }
